c49d5c59eb00f5473bdfc77bf64ce830aed6f756
Samah Farez <97588511+SamahFarez@users.noreply.github.com>
2024-12-13 23:40:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  console.log("Session ID:", req.sessionID);
+  console.log("Session Data:", req.session);
+  next();
+});
+
+
 // MongoDB Connection
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
